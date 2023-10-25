c96ddafda3568de8ac300401c100c5b308f0b40c
Fitrah Firdaus <fitrah.firdaus@gmail.com>
2023-10-25 11:10:15 +0700

    Adding Select Data

--- src/test/java/com/smk/cashier/service/BarangServiceTest.java
+++ src/test/java/com/smk/cashier/service/BarangServiceTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.TestMethodOrder;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -96,6 +98,29 @@ class BarangServiceTest {
         laptopGaming.setDateCreated(new Date());
         laptopGaming.setLastModified(new Date());
         barangDao.save(laptopGaming);
+    }
+
+    @Test
+    @Order(5)
+    void getDataById() {
+        BarangDao barangDao = new BarangDao();
+        Optional<Barang> barang1 = barangDao.get(1);
+        barang1.ifPresent(new Consumer<Barang>() {
+            @Override
+            public void accept(Barang barang) {
+                assertEquals("Laptop", barang.getNamaBarang());
+                assertEquals("LP001", barang.getKodeBarang());
+            }
+        });
+
+        Optional<Barang> barang2 = barangDao.get(2);
+        barang2.ifPresent(new Consumer<Barang>() {
+            @Override
+            public void accept(Barang barang) {
+                assertEquals("Mouse", barang.getNamaBarang());
+                assertEquals("MO001", barang.getKodeBarang());
+            }
+        });
     }
 
 
